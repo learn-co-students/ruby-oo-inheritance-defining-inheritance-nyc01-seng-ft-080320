5efc8357f869506ab7950b3cabd7e3597187b399
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-08 11:07:01 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,24 @@
 require_relative "./vehicle.rb"
+class Car < Vehicle
+    #attrs section
+    attr_accessor :make
+
+    # initialization
+    # def initialize (wheel_size, wheel_number, make)
+    #     super(wheel_size, wheel_number)
+    #     self.make = make
+    # end
+
+    def go
+        # super.go
+        # puts "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
+
+#class Viking < Person
+#def initialize(name, age, weapon)
+   # super(name, age)
+   # @weapon = weapon
+  #end
+#end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
 class Vehicle
+    # attrs section
+    attr_accessor :wheel_size, :wheel_number
 
+    # Initialize method for creating and initializing new instances
+    def initialize(wheel_size, wheel_number)
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
 end
