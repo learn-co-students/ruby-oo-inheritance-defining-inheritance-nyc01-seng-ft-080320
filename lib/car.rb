4176b9551a350cfe5be9b6b0ade08235c08438c9
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-10 00:09:12 -0400

    lab completed

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" #the Car class needs access to the Vehicle class stored in a different file in this directory. Thus, it needs to be required. 
+
+class Car < Vehicle #the '<' symbol shows that Car "inherits" its super class, Vehicle
+    #although there are no methods explicitly written in Car, it inherited all of the methods of Vehicle. Each new instance of Car will, just like every Vehicle, be born knowing its wheel_size and wheel_number, as well as being able to .go and .fill_up_tank
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+    #if user needs to overricde a method in the subclass, just rewrite the method. the inheritance will no longer be the behavior that ruby executes; instead, the newly defined behavior in the subclass is used. 
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,21 @@
+#parent; "superclass"
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize wheel_size, wheel_number 
+        @wheel_size = wheel_size 
+        @wheel_number = wheel_number
+    end
+    #every instance of Vehicle – every car – will be born with knowing its wheel_size and wheel_number (provided by the user when creating it).
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
+
+    #every car will also have access to these methods: .go & .fill_up_tank – "behaviors" that all cars can exhibit through the inheritance of the aforementioned methods.
 
 end
